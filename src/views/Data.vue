3112ed48772ae224cfa394493e92196e66578238
Chris <chrispanetta123@gmail.com>
2019-10-27 19:54:28 +1100

    retrieving data

--- src/views/Data.vue
+++ src/views/Data.vue
@@ -1,15 +1,29 @@
 <template>
 	<div class="home">
 		<h1>The Data</h1>
+		{{parkingData}}
 	</div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import Home from "@/components/Home.vue";
+import axios from "axios";
 
 export default {
 	name: "data",
-	components: {}
+	components: {},
+	data() {
+		return {
+			parkingData: null
+		};
+	},
+	methods: {},
+	created() {
+		axios
+			.get("https://data.melbourne.vic.gov.au/resource/vh2v-4nfs.json")
+			.then(response => (this.parkingData = response.data))
+			.catch(error => console.log(error));
+	}
 };
 </script>
